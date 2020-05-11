2c40256b32e37929b3e9390864843d2c0131ddb1
Matt Welte <matthew.welte95@gmail.com>
2020-05-11 00:13:10 -0400

    working success unit test

--- server/middleware/initialize.go
+++ server/middleware/initialize.go
@@ -15,51 +15,53 @@ import (
 const mongoURI = "mongodb://127.0.0.1:27018/"
 const dbName = "bitboard-dev"
 
-var client *mongo.Client
-
-type dBInitializer struct {
-	client        *mongo.Client
-	clientOptions *options.ClientOptions
-	logger        *log.Logger
+type mongoConnection struct{}
+type serverConnector interface {
+	connectToServer(ctx context.Context, mongoClient *mongo.Client) error
+	testServerConnection(ctx context.Context, mongoClient *mongo.Client) error
+	getLogger() *log.Logger
 }
 
-func init() {
-	logger := log.New(os.Stdout, "SERVER: ", log.Ldate|log.Ltime|log.Lshortfile)
-	clientOptions := options.Client().ApplyURI(mongoURI)
-	client = initMongoClient(client, clientOptions, logger)
+var mongoConnector serverConnector
+
+// MongoDatabase returns the singleton database instance
+func MongoDatabase() *mongo.Database {
+	mongoConnector = mongoConnection{}
+	client := initialize(options.Client().ApplyURI(mongoURI), mongoConnector)
+	return client.Database(dbName)
 }
 
-func (initializer dBInitializer) initialize() *mongo.Client {
-	var err error
-	mongoClient, err = mongo.NewClient(clientOptions)
+func initialize(clientOptions *options.ClientOptions, serverConnectorHelper serverConnector) *mongo.Client {
+	logger := serverConnectorHelper.getLogger()
+	mongoClient, err := mongo.NewClient(clientOptions)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 
-	if err := connectToServer(ctx, mongoClient); err != nil {
+	logger.Println("Passed ctx")
+	if err := serverConnectorHelper.connectToServer(ctx, mongoClient); err != nil {
 		logger.Fatal(err)
 	}
 
 	logger.Println("Attempting to connect to mongo server...")
-	if err := testServerConnection(ctx, mongoClient); err != nil {
+	if err := serverConnectorHelper.testServerConnection(ctx, mongoClient); err != nil {
 		logger.Fatal(err)
 	}
 	logger.Println("Successfully Connected to MongoDB")
 	return mongoClient
 }
 
-func connectToServer(ctx context.Context, mongoClient *mongo.Client) error {
+func (m mongoConnection) getLogger() *log.Logger {
+	return log.New(os.Stdout, "SERVER: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func (m mongoConnection) connectToServer(ctx context.Context, mongoClient *mongo.Client) error {
 	return mongoClient.Connect(ctx)
 }
 
-func testServerConnection(ctx context.Context, mongoClient *mongo.Client) error {
+func (m mongoConnection) testServerConnection(ctx context.Context, mongoClient *mongo.Client) error {
 	return mongoClient.Ping(ctx, nil)
-}
-
-// MongoDatabase returns the singleton database instance
-func MongoDatabase() *mongo.Database {
-	return client.Database(dbName)
 }
--- server/middleware/initialize_test.go
+++ server/middleware/initialize_test.go
@@ -1,1 +1,55 @@
 package middleware
+
+import (
+	"context"
+	"io/ioutil"
+	"log"
+	"testing"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type successMongoConnection struct{}
+
+func discardLogger() *log.Logger {
+	return log.New(ioutil.Discard, "", 0)
+}
+
+func (m successMongoConnection) connectToServer(ctx context.Context, mongoClient *mongo.Client) error {
+	return nil
+}
+
+func (m successMongoConnection) testServerConnection(ctx context.Context, mongoClient *mongo.Client) error {
+	return nil
+}
+
+func (m successMongoConnection) getLogger() *log.Logger {
+	return discardLogger()
+}
+
+func TestInitializeSuccess(t *testing.T) {
+	var mockMongoConnector serverConnector
+	mockMongoConnector = successMongoConnection{}
+	initialize(options.Client().ApplyURI(mongoURI), mockMongoConnector)
+}
+
+type failServerInitMongoConnection struct{}
+
+func (m failServerInitMongoConnection) connectToServer(ctx context.Context, mongoClient *mongo.Client) error {
+	return errors.new("type")
+}
+
+func (m failServerInitMongoConnection) testServerConnection(ctx context.Context, mongoClient *mongo.Client) error {
+	return nil
+}
+
+func (m failServerInitMongoConnection) getLogger() *log.Logger {
+	return discardLogger()
+}
+
+func TestInitializeFailure(t *testing.T) {
+	var mockMongoConnector serverConnector
+	mockMongoConnector = mockMongoConnection{}
+	initialize(options.Client().ApplyURI(mongoURI), mockMongoConnector)
+}
