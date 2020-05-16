23896f964955d1106f645b7aa35e85afa994e6a6
Matthew Welte <matthew.welte95@gmail.com>
2020-05-16 17:51:52 -0400

    pushing

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const baseUrl = "http://localhost:8080"
+const BASE_URL = "http://localhost:8080"
 
 const instance = axios.create({
-    baseURL: baseUrl
+    baseURL: BASE_URL
 });
 
 export default instance;
