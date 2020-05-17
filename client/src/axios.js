7aa0c4fa85b9f5ffae39f53a4ac2fe76d4ddc2fb
Matthew Welte <matthew.welte95@gmail.com>
2020-05-17 13:38:10 -0400

    port number cleanup

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:8080"
+const BASE_URL = "http://172.19.0.1:8500"
 
 const instance = axios.create({
     baseURL: BASE_URL
 });
+
+console.log(BASE_URL)
 
 export default instance;
